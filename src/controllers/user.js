fe5afab3f50f40b6d9f19d4826022f3f8a9a6a37
zcong1993 <1005997919@qq.com>
2017-05-19 11:16:32 +0800

    find user by _id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,14 @@
 import User from '../models/user'
+import { sendError } from '../common/helpers'
 
 export async function me(ctx) {
-  const user = await User.findOne({ apiKey: ctx.state.user.apiKey })
+  // ctx.state.user.apiKey is undefined
+  // db.user.apiKey is also undefined
+  const user = await User.findOne({ _id: ctx.state.user._id })
     .select('username email createdAt updatedAt')
     .exec()
+  if (!user) {
+    return sendError(ctx, 'Authorization failed', 401)
+  }
   ctx.body = user
 }
