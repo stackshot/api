1028348cf3b2a09a68350b507b1f4941e08cabe6
EGOIST <0x142857@gmail.com>
2017-05-26 12:03:50 +0800

    fix lint

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import bodyparser from 'koa-bodyparser'
 import jwt from 'koa-jwt'
 import convert from 'koa-convert'
 import cors from 'kcors'
-import {publicKey} from './common/helpers'
+import { publicKey } from './common/helpers'
 
-import './db'
+import './db' // eslint-disable-line import/no-unassigned-import
 
 import publicRouter from './routers/public-router'
 import privateRouter from './routers/private-router'
@@ -21,21 +21,25 @@ export default () => {
   app.use(async (ctx, next) => {
     try {
       await next() // Attempt to go through the JWT Validator
-    } catch (e) {
-      if (e.status == 401 ) {
+    } catch (err) {
+      if (err.status === 401) {
         // Prepare response to user.
-        ctx.status = e.status
-        ctx.body = {error: 'You don\'t have a signed token dude :('}
+        ctx.status = err.status
+        ctx.body = { error: "You don't have a signed token dude :(" }
       } else {
-        throw e // Pass the error to the next handler since it wasn't a JWT error.
+        throw err // Pass the error to the next handler since it wasn't a JWT error.
       }
     }
   })
 
-  app.use(convert(jwt({
-    secret: publicKey,
-    algorithm: 'RS256'
-  })))
+  app.use(
+    convert(
+      jwt({
+        secret: publicKey,
+        algorithm: 'RS256'
+      })
+    )
+  )
 
   app.use(privateRouter.routes())
   app.use(privateRouter.allowedMethods())
